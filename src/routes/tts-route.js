cd0d8d910e202aa49c76f26ffa00a4799c4226e3
Haejeong079 <goesed7582@naver.com>
2024-11-22 13:41:38 +0900

    FIX, dir

--- src/routes/tts-route.js
+++ src/routes/tts-route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ttsController = require('../controller/tts-controller');
+const ttsController = require('../controllers/tts-controller');
 
 // 경로 안내 음성 파일 생성 API
 router.post('/audio', ttsController.getRouteAudio);
