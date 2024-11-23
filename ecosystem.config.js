c1f12b0facf37791d65d187f438329c7ae2608ea
Haejeong079 <goesed7582@naver.com>
2024-11-24 03:17:07 +0900

    cicd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,14 +11,13 @@ module.exports = {
       },
     ],
     deploy: {
-        production: {
-          user: "ubuntu", // 서버의 사용자 이름
-          host: "13.209.156.54", // 서버의 IP 주소
-          ref: "origin/main", // 배포할 브랜치
-          repo: "git@github.com:9oormthon-univ/2024_DANPOONG_TEAM_40_BE.git", // Git 저장소
-          path: "/var/www/moduegil-be", // 서버의 애플리케이션 경로
-          "post-deploy":
-            "npm install && pm2 reload ecosystem.config.js --env production",
-        },
+      production: {
+        user: "ubuntu", // 서버의 사용자 이름
+        host: "3.37.95.121", // 서버의 IP 주소 (http:// 제거)
+        ref: "origin/main", // 배포할 브랜치
+        repo: "git@github.com:9oormthon-univ/2024_DANPOONG_TEAM_40_BE.git", // Git 저장소
+        path: "/var/www/moduegil-be", // 서버의 애플리케이션 경로
+        "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production",
       },
-    };
+    },
+  };
